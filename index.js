dd43149fd20b1e59c05660727aeab259a76dacec
mark novelero <mark.novelero@gmail.com>
2021-04-22 11:54:49 -0400

    4/22

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 let mainContainer = document.querySelector("div.candidates")
 
+let theForm = document.querySelector('form.user_comment')
+let textAreaValue = document.querySelector('textarea')
+let ulSmackTalk = document.querySelector('ul.comments')
+
+let copyObj = {}
 
 fetch("http://localhost:3000/ballers")
 .then (res => res.json())
 .then (ballers => {
+    console.log(ballers)
     ballers.forEach(hoopObj => {
         ballerfy(hoopObj)        
     })
+    copyObj = ballers
 })
 
 
@@ -50,38 +57,55 @@ function ballerfy (obj){
          .then (updatedObj => {
              voteCount.innerText = updatedObj.likes
              obj.likes = updatedObj.likes
-             voteButton.innerText = "THANK YOU"
+             voteButton.innerText = "THANKS!"
              voteButton.disabled = true
          })        
     })
 }
 
-// let theForm = document.querySelector('form.user_comment')
-// let textAreaValue = document.querySelector('textarea')
-// let ulSmackTalk = document.querySelector('ul.comments')
 
-// theForm.addEventListener('submit' , (evt)=>{
 
-//     evt.preventDefault()
-//     let userSmack = textAreaValue.value
 
-//      fetch("http://localhost:3000/ballers/", {
-//          method: "POST",
-//          headers: {
-//            "Content-Type": "application/json",
-//          },
-//          body: JSON.stringify({
-//              reasons: userSmack,
-//              id: 1
-//          }),
-//        })
-//        .then (res => res.json())
-//        .then (mjObj => {
-//           let newLi = document.createElement('li')
-//            newLi.innerText = mjObj.reasons
-//            ulSmackTalk.append(newLi)
-//        })
-//      })
+
+theForm.addEventListener('submit' , (evt)=>{
+    evt.preventDefault()
+    let userComment = textAreaValue.value
+
+     fetch("http://localhost:3000/comments", {
+         method: "POST",
+         headers: {
+           "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+             comment: userComment
+         }),
+       })
+       .then(res => res.json())
+       .then(newObj => {
+           let blankLi = document.createElement('li')
+           blankLi.innerText= newObj.comment
+           ulSmackTalk.append(blankLi)
+       })
+    })
+
+    fetch("http://localhost:3000/comments")
+    .then (res => res.json())
+    .then (comment => {
+    comment.forEach(userComment => {
+    let newLi = document.createElement('li')
+    newLi.innerText = userComment.comment
+    ulSmackTalk.append(newLi) 
+    })
+})
+ 
+
+
+
+    
+   
+  
+
+  
 
 
 
